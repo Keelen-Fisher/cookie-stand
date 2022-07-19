2cc6423a3038b8d20cf9c26c9beab2367b4e7807
Keelen Fisher <keelencarrera@gmail.com>
2022-07-18 20:46:13 -0500

    added render function and call functions

--- js/app.js
+++ js/app.js
@@ -9,8 +9,10 @@ let seattle = {
   minCustomer: 23,
   maxCustomer: 65,
   avgCookieBought: 6.3, 
-  randomCustomerPerHour: function(){
-    return Math.floor(Math.random() * (this.maxCustomer -this.minCustomer + 1) + this.minCustomer);},
+  randomCustomerPerHour: function()
+  {
+    return Math.floor(Math.random() * (this.maxCustomer -this.minCustomer + 1) + this.minCustomer);
+  },
     // This Code was from mdn webdocs
   cookiesBoughtPerHour: [], 
   totalDailyCookies: 0,
@@ -19,24 +21,52 @@ let seattle = {
   cookiesHour: function(){ for(let i = 0; i < hours.length; i++)
     {
       //  using avg and randCust - calc a number calculated number: avgCookieBought * randomcustomer
-      let simCookies = this.avgCookieBought * randomCustomerPerHour();
+      let simCookies = Math.ceil(this.avgCookieBought * this.randomCustomerPerHour());
       // add that calculation to the array by using this.cookiesBoughtPerHour.push(calculated number)
       this.cookiesBoughtPerHour.push(simCookies);
       // Add all of the cookies together.
       this.totalDailyCookies += simCookies
     }
   },
+// Rendering itself to the HTML Document: Dom Manipulation pt 2
 
+render: function(){
+  this.cookiesHour();
+  // CREATING ARTICLE:
+  let articleElem = document.createElement('article');
+  console.log(this.cookiesBoughtPerHour);
+  // Adding this to the HTML document:
+  cookiesSale.appendChild(articleElem);
+
+  // CREATING H2
+  let h2Elem = document.createElement('h2');
+
+  // Adding: The City name
+  h2Elem.textContent = this.city;
+  articleElem.appendChild(h2Elem);
+
+  let ulElem = document.createElement('ul');
+  articleElem.appendChild(ulElem);
+
+  for(let i = 0; i < this.cookiesBoughtPerHour.length; i++){
+    let liElem = document.createElement('li');
+    liElem.textContent = `${hours[i]}: ${this.cookiesBoughtPerHour[i]} Cookies!`;
+    ulElem.appendChild(liElem);
+  }
   
-  
+  let liElem = document.createElement('li');
+  liElem.textContent = `Total: ${this.totalDailyCookies} Cookies!`;
+  ulElem.appendChild(liElem);
 
+  }
 
-
-
-
-}
+};
 
 console.log(seattle.randomCustomerPerHour());
 console.log(seattle.cookiesHour);
- console.log(seattle.cookiesBoughtPerHour);
- console.log(seattle.totalDailyCookies)
+console.log(seattle.cookiesBoughtPerHour);
+console.log(seattle.totalDailyCookies);
+
+seattle.randomCustomerPerHour();
+seattle.render();
+console.log(seattle);
